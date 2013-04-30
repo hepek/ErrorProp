95d6007bc6b27a2e82a2ec86bfae8af17efd1a10
hepek <zivotinja@gmail.com>
2013-04-30 14:53:59 +0200

    exports, base dependency

--- src/Math/ErrorProp.hs
+++ src/Math/ErrorProp.hs
@@ -3,14 +3,14 @@
 -- | Module 'ErrorProp' is used to calculate error propagation in
 --   linear and non-linear systems
 module Math.ErrorProp
-{-       (Measurement
+       (Measurement
         , Fn
         , um, cm
         , lt, nt
         , x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11
         , transform
-        , diag, trans, (>.), (><)
-       ) -}
+--        , diag, trans, (>.), (><)
+       )
        where
 
 import Data.List
