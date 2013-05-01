14dfe04e045ae9c46c82f164f89d8f5041cdd343
Milan Marković <zivotinja@gmail.com>
2013-05-01 20:23:13 +0200

    jacobian now supports all kinds of variables

--- src/Math/ErrorProp.hs
+++ src/Math/ErrorProp.hs
@@ -1,5 +1,4 @@
 {-# language ParallelListComp #-}
-
 -- | Module 'ErrorProp' is used to calculate error propagation in
 --   linear and non-linear systems
 module Math.ErrorProp
@@ -9,7 +8,7 @@ module Math.ErrorProp
         , lt, nt
         , x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11
         , transform
---        , diag, trans, (>.), (><)
+        , diag, takeDiag, trans, (>.), (><)
        )
        where
 
@@ -18,9 +17,6 @@ import Data.List.Split
 import Control.Applicative
 
 import Math.Symbolic
-
-type Mx  = [[Double]]
-type Vec = [Double]
 
 type Fn = Expr Double
 
@@ -32,6 +28,9 @@ data Transf = Lt Mx            -- ^ linear transformations
 --  covariance matrix
 data Measurement = Measurement Vec Mx
               deriving (Eq)
+
+type Mx  = [[Double]]
+type Vec = [Double]
 
 -- | Matrix vector multiplication
 (>.) :: (Num a) => [[a]] -> [a] -> [a]
@@ -104,6 +103,9 @@ lt = Lt
 xs@[x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11] =
   map (Symbol.('x':).show) [1..11] :: [Fn]
 
+ts@[t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11] =
+  map (Symbol.('t':).show) [1..11] :: [Fn]
+
 -- | Smart constructor of nonlinear transformation
 --   e.g. nt [x1*x1, x2, sin(x3)]
 nt :: [Fn]       -- ^ A list of functions, one for each output parameter
@@ -115,24 +117,29 @@ jacobian :: [Fn] -> [[Fn]]
 jacobian fs = transpose [map d fs | d <- ds]
   where
     ds = [ simplify.(diff s) | s <- xs']
-    xs' = takeWhile (\(Symbol x) -> x <= maxDegreeX fs) xs
+    xs' = variablesOf fs
 
-maxDegreeX fs = l . sort . map (l.sort.hx) $ fs
+-- | extracts variables from a list of expressions
+variablesOf :: [Fn] -> [Fn]
+variablesOf fs =  
+    (nubBy$ \a b -> cmpSymbol a b == EQ) .
+    sortBy cmpSymbol .
+    concatMap ex $ fs
   where
-    l [] = "x1"
-    l xs = last xs
+    ex (Symbol s) = [Symbol s]
+    ex (Sum a b)  = (ex a) ++ (ex b)
+    ex (Prod a b) = (ex a) ++ (ex b)
+    ex (Exp a b)  = (ex a) ++ (ex b)
+    ex (Log a)    = ex a
+    ex (Sin a)    = ex a
+    ex (Cos a)    = ex a
+    ex (Rec a)    = ex a
+    ex (Neg a)    = ex a
+    ex (Atom _)   = []
 
-hx x@(Symbol s) | x `elem` xs = [s]
-                | otherwise   = []
-hx (Sum a b)  = (hx a) ++ (hx b)
-hx (Prod a b) = (hx a) ++ (hx b)
-hx (Exp a b)  = (hx a) ++ (hx b)
-hx (Log a)    = hx a
-hx (Sin a)    = hx a
-hx (Cos a)    = hx a
-hx (Rec a)    = hx a
-hx (Neg a)    = hx a
-hx (Atom _) = []
+    cmpSymbol (Symbol s1) (Symbol s2) = compare s1 s2
+    cmpSymbol _ _ = error "Attempt to compare non-symbol."
+
 
 -- | evaluate non-linear transformation at operation point
 --   represented by vector t and a measurement
