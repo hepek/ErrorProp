c8b874acbe7ac891986d0feede75941edbfc67be
hepek <zivotinja@gmail.com>
2013-05-07 16:42:56 +0200

    exported partial, some fixes

--- src/Math/ErrorProp.hs
+++ src/Math/ErrorProp.hs
@@ -11,6 +11,7 @@ module Math.ErrorProp
         , x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11
         , transform
         , diag, takeDiag, trans, (>.), (><)
+        , partial
         , simplify
        )
        where
